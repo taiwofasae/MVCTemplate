33f768b6e347cdc723353de23b8bf91b489e1c04
Taiwo Fasae <taiwofasae@gmail.com>
2017-06-15 11:16:32 +0100

    Initial

---
+++ src/__NAME__/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(__NAME__.Startup))]
+namespace __NAME__
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
